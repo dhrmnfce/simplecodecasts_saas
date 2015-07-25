29ef5ba85b2690b0d8882a967aff4ba5b3d57be9
Dylan Ludwick <dylanludwick@gmail.com>
2015-07-25 18:16:13 +0000

    Modified contact mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'dylanludwick#gmail.com'
+    default to: 'dylanludwick@gmail.com'
     def contact_email(name, email, body)
        @name = name
        @email = email
